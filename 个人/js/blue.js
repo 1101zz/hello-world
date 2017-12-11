ea865fd4cf3c5bcb542f7d27b04aebd91c950861
zzd <1569177304@qq.com>
2017-12-11 11:16:21 +0800

    kn//

--- 个人/js/blue.js
+++ 个人/js/blue.js
@@ -9,18 +9,18 @@ requirejs.config({
 requirejs(["jquery","xiangmu"],function ($,xiangmu) {
    var shuxing={
         img:"img/5.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"购物车",
+        main:"简单的vue，实现请求json文件渲染页面",
         shuxing:"123all",
-        dizhi:"小车.html"
+        dizhi:"car.html"
     };
     var asd=new xiangmu(shuxing);
     asd.init();
     var shuxing1={
         lie:"one",
         img:"img/5.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"特效照片墙",
+        main:"通过css，jq实现动态照片墙特效",
         shuxing:"456all",
         dizhi:"photo.html"
     };
@@ -29,8 +29,8 @@ requirejs(["jquery","xiangmu"],function ($,xiangmu) {
     var shuxing2={
         lie:"one",
         img:"img/6.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"放大镜",
+        main:"简易放大镜，用require模块化，通过改变参数实现不同尺寸放大镜",
         shuxing:"123all",
         dizhi:"remagnifer.html"
     };
@@ -39,60 +39,60 @@ requirejs(["jquery","xiangmu"],function ($,xiangmu) {
     var shuxing3={
         lie:"one",
         img:"img/5.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"3d旋转",
+        main:"主要是通过css3实现球体特效",
         shuxing:"456all",
-        dizhi:"球体实验.html"
+        dizhi:"3dtranstion.html"
     };
     var asd3=new xiangmu(shuxing3);
     asd3.init();
     var shuxing4={
         lie:"one",
         img:"img/7.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"轮播图",
+        main:"简单的轮播图特效",
         shuxing:"789all",
-        dizhi:"轮播.html"
+        dizhi:"lunbo.html"
     };
     var asd4=new xiangmu(shuxing4);
     asd4.init();
     var shuxing5={
         lie:"one",
         img:"img/5.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"百分比进度条",
+        main:"通过画布实现动态画圆",
         shuxing:"123all",
-        dizhi:"index.html"
+        dizhi:"baifenbi.html"
     };
     var asd5=new xiangmu(shuxing5);
     asd5.init();
     var shuxing6={
         lie:"one",
         img:"img/5.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"js小游戏",
+        main:"通过css3样式与js实现类似于连连看的小游戏",
         shuxing:"789all",
-        dizhi:"index.html"
+        dizhi:"fanpaizi.html"
     };
     var asd6=new xiangmu(shuxing6);
     asd6.init();
     var shuxing7={
         lie:"one",
         img:"img/3.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"模拟时钟",
+        main:"通过css样式，js实现简易时钟",
         shuxing:"456all",
-        dizhi:"index.html"
+        dizhi:"biaozi.html"
     };
     var asd7=new xiangmu(shuxing7);
     asd7.init();
     var shuxing8={
         lie:"one",
         img:"img/3.jpg",
-        title:"asldjlsa",
-        main:"aslkdjlaskhflksajclvnsvlavj",
+        title:"旋转粒子",
+        main:"h5画布特效",
         shuxing:"123all",
-        dizhi:"index.html"
+        dizhi:"xuanzhuanlizi.html"
     };
     var asd8=new xiangmu(shuxing8);
     asd8.init();
